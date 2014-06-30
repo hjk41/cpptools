ebb9af9f3db048a4aa044cc6a21f04a6931e0175
Chuntao.hong <chuntao.hong@gmail.com>
2014-06-30 14:49:51 +0800

    Update logging.h

--- logging.h
+++ logging.h
@@ -6,44 +6,53 @@
 #include <stdarg.h>
 #include <assert.h>
 
-namespace TinyRPC
-{
 
-enum LogPriority
+
+enum LogLevel
 {
-	LOG_NORMAL,
-	LOG_NOTICE,
-	LOG_WARNING,
-	LOG_ERROR
+    LOG_INFO,
+    LOG_WARNING,
+    LOG_ERROR
 };
 
-inline void TinyLog(LogPriority code, const char* format, ... ) {
+inline void OctopusLog(LogLevel level,
+    const char * component,
+    const char * filename,
+    int lineNum,
+    const char * format, ...) 
+{
     va_list args;
-    fprintf( stdout, "TinyLog: " );
-    va_start( args, format );
-    vfprintf( stdout, format, args );
-    va_end( args );
-    fprintf( stdout, "\n" );
-	fflush(stdout);
-	if (code == LOG_ERROR)
-		assert(0);
+    switch (level)
+    {
+    case LOG_INFO:
+        fprintf(stdout, "OctopusLog[%s][%s:%d]: ", component, filename, lineNum);
+        break;
+    case LOG_WARNING:
+        fprintf(stdout, "OctopusWarn[%s][%s:%d]: ", component, filename, lineNum);
+        break;
+    case LOG_ERROR:
+        fprintf(stdout, "OctopusErr[%s][%s:%d]: ", component, filename, lineNum);
+        break;
+    }    
+    va_start(args, format);
+    vfprintf(stdout, format, args);
+    va_end(args);
+    fprintf(stdout, "\n");
+    fflush(stdout);
+    if (level == LogLevel::LOG_ERROR)
+    {
+        std::abort();
+    }
 }
 
-inline void TinyAssert(bool predicate, const char* format, ... ) {
-#ifdef _DEBUG
-	if (!predicate)
-	{
-		va_list args;
-		fprintf( stderr, "TinyLog: " );
-		va_start( args, format );
-		vfprintf( stderr, format, args );
-		va_end( args );
-		fprintf( stderr, "\n" );
-		assert(0);
-		fflush(stderr);
-	}
-#endif
-}
+#define LOG(format, ...) \
+do{ OctopusLog(LOG_INFO, LOGGING_COMPONENT, __FILE__, __LINE__, (format), ##__VA_ARGS__); } while (0)
 
+#define WARN(level, format, ...) \
+do{ OctopusLog(LOG_WARNING, LOGGING_COMPONENT, __FILE__, __LINE__, (format), ##__VA_ARGS__); } while (0)
 
-};
+#define ABORT(level, format, ...) \
+do{ OctopusLog(LOG_ERROR, LOGGING_COMPONENT, __FILE__, __LINE__, (format), ##__VA_ARGS__); } while (0)
+
+#define ASSERT(pred, ...) \
+do{ if (!(pred)) ABORT(##__VA_ARGS__); } while (0)
